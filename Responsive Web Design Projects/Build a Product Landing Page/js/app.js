32416dcdf1fc4adbfce659ee5167407969a268ad
Ruben Simon <info@rubensimon.de>
2020-08-01 13:06:44 +0200

    added js for adjusting the + and - in accordion if collapsed or expanded

--- Responsive Web Design Projects/Build a Product Landing Page/js/app.js
+++ Responsive Web Design Projects/Build a Product Landing Page/js/app.js
@@ -3,6 +3,27 @@ const acc = document.getElementsByClassName("menu-item__text");
 for(i=0;i<acc.length;i++){
       acc[i].addEventListener("click",() =>{
             let target = event.target.parentElement.parentElement.childNodes;
+            /*change between + and - when collapsed or expanded*/
+            for(i of target) {
+                  if(i.className ==="menu-item") {
+                        let child = i.childNodes;
+                        for (j of child) {
+                              if((j.className === "menu-item__text button" && j.getAttribute("data-button") === "+")) {
+                                    j.innerHTML = "-";
+                                    j.setAttribute("data-button", "-");
+                                    
+                              }
+                              else if (j.className === "menu-item__text button" && j.getAttribute("data-button") === "-") {
+                                    j.innerHTML = "+";
+                                    j.setAttribute("data-button", "+");
+                              }
+
+                        }
+                        
+                  }
+                  
+            }
+            
             for (i of target){
             
                   if(i.tagName && i.className.includes("carousel") ){
@@ -19,8 +40,7 @@ let svg = document.getElementsByClassName("check__svg");
 
 for (i of svg){
       i.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-      i.setAttribute("width", "24");
-      i.setAttribute("height", "24");
+  
 }
 
 
